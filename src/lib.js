ed39f89d56211154230cdf0d478ff69aac8a5444
supernovel <ekfueh@naver.com>
2017-08-21 06:59:16 +0900

    naturalSort 변경

--- src/lib.js
+++ src/lib.js
@@ -179,122 +179,61 @@ Lib.isArray = function(arr){
     return false;
 }
 
-var rx = /(\d+)|(\D+)/g,
-    rd = /\d/,
-    rz = /^0/;
+/*
+ * Natural Sort algorithm for Javascript - Version 0.8.1 - Released under MIT license
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ */
 
-Lib.naturalSort = (function() {
-  return function(as, bs) {
-    var a, a1, b, b1, nas, nbs;
-    
-    if ((bs != null) && (as == null)) {
-      return -1;
+Lib.naturalSort = function naturalSort (a, b) {
+    var re = /(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g,
+        sre = /^\s+|\s+$/g,   // trim pre-post whitespace
+        snre = /\s+/g,        // normalize all whitespace to single ' ' character
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        i = function(s) {
+            return (naturalSort.insensitive && ('' + s).toLowerCase() || '' + s).replace(sre, '');
+        },
+        // convert all to strings strip whitespace
+        x = i(a),
+        y = i(b),
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre), 16) || (xN.length !== 1 && Date.parse(x)),
+        yD = parseInt(y.match(hre), 16) || xD && y.match(dre) && Date.parse(y) || null,
+        normChunk = function(s, l) {
+            // normalize spaces; find floats not starting with '0', string or 0 if not defined (Clint Priest)
+            return (!s.match(ore) || l == 1) && parseFloat(s) || s.replace(snre, ' ').replace(sre, '') || 0;
+        },
+        oFxNcL, oFyNcL;
+    // first try and sort Hex codes or Dates
+    if (yD) {
+        if (xD < yD) { return -1; }
+        else if (xD > yD) { return 1; }
     }
-
-    if ((as != null) && (bs == null)) {
-      return 1;
-    }
-
-    if (typeof as === "number" && isNaN(as)) {
-      return -1;
-    }
-    
-    if (typeof bs === "number" && isNaN(bs)) {
-      return 1;
-    }
-
-    nas = +as;
-    nbs = +bs;
-
-    if (nas < nbs) {
-      return -1;
-    }
-
-    if (nas > nbs) {
-      return 1;
-    }
-
-    if (typeof as === "number" && typeof bs !== "number") {
-      return -1;
-    }
-
-    if (typeof bs === "number" && typeof as !== "number") {
-      return 1;
-    }
-
-    if (typeof as === "number" && typeof bs === "number") {
-      return 0;
-    }
-
-    if (isNaN(nbs) && !isNaN(nas)) {
-      return -1;
-    }
-
-    if (isNaN(nas) && !isNaN(nbs)) {
-      return 1;
-    }
-
-    a = String(as);
-    b = String(bs);
-
-    if (a === b) {
-      return 0;
-    }
-
-    if (!(rd.test(a) && rd.test(b))) {
-      return (a > b ? 1 : -1);
-    }
-
-    a = a.match(rx);
-    b = b.match(rx);
-
-    while (a.length && b.length) {
-        a1 = a.shift();
-        b1 = b.shift();
-        if (a1 !== b1) {
-            if (rd.test(a1) && rd.test(b1)) {
-                return a1.replace(rz, ".0") - b1.replace(rz, ".0");
-            } else {
-                return (a1 > b1 ? 1 : -1);
-            }
+    // natural sorting through split numeric strings and default strings
+    for(var cLoc = 0, xNl = xN.length, yNl = yN.length, numS = Math.max(xNl, yNl); cLoc < numS; cLoc++) {
+        oFxNcL = normChunk(xN[cLoc] || '', xNl);
+        oFyNcL = normChunk(yN[cLoc] || '', yNl);
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) {
+            return isNaN(oFxNcL) ? 1 : -1;
         }
+        // if unicode use locale comparison
+        if (/[^\x00-\x80]/.test(oFxNcL + oFyNcL) && oFxNcL.localeCompare) {
+            var comp = oFxNcL.localeCompare(oFyNcL);
+            return comp / Math.abs(comp);
+        }
+        if (oFxNcL < oFyNcL) { return -1; }
+        else if (oFxNcL > oFyNcL) { return 1; }
     }
-    return a.length - b.length;
-  };
-})();
-
-Lib.sortAs = function(order) {
-  var i, l_mapping, mapping, x;
-  mapping = {};
-  l_mapping = {};
-  for (i in order) {
-    x = order[i];
-    mapping[x] = i;
-    if (typeof x === "string") {
-      l_mapping[x.toLowerCase()] = i;
-    }
-  }
-  return function(a, b) {
-    if ((mapping[a] != null) && (mapping[b] != null)) {
-      return mapping[a] - mapping[b];
-    } else if (mapping[a] != null) {
-      return -1;
-    } else if (mapping[b] != null) {
-      return 1;
-    } else if ((l_mapping[a] != null) && (l_mapping[b] != null)) {
-      return l_mapping[a] - l_mapping[b];
-    } else if (l_mapping[a] != null) {
-      return -1;
-    } else if (l_mapping[b] != null) {
-      return 1;
-    } else {
-      return Lib.naturalSort(a, b);
-    }
-  };
-};
+}
 
 Lib.getSort = function(sorters, attr) {
     var sort;
+
     if (sorters != null) {
         if (Lib.isFunction(sorters)) {
             sort = sorters(attr);
@@ -305,6 +244,7 @@ Lib.getSort = function(sorters, attr) {
             return sorters[attr];
         }
     }
+    
     return Lib.naturalSort;
 };
 
